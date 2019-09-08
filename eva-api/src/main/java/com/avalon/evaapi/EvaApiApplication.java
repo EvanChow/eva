1313462bf604f704b60bcd5fceffd8bfe8038486
EvanChow <evanchow1993@gmail.com>
2019-09-08 22:14:02 +0800

    23333

---
+++ eva-api/src/main/java/com/avalon/evaapi/EvaApiApplication.java
@@ -0,0 +1,13 @@
+package com.avalon.evaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaApiApplication.class, args);
+    }
+
+}
